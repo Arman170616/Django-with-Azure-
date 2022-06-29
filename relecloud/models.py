e430f80c0db6093704ac1519ba21a8ab9039072d
Arman170616 <armanice09@gmail.com>
2022-06-30 02:13:46 +0600

    Declaring models

--- relecloud/models.py
+++ relecloud/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=False, blank=False)
