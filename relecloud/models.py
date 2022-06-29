d1219c05e1835352b8253698f4c7a5a967143101
Arman170616 <armanice09@gmail.com>
2022-06-30 02:41:11 +0600

    creating relations

--- relecloud/models.py
+++ relecloud/models.py
@@ -6,3 +6,16 @@ class Destination(models.Model):
     name = models.CharField(max_length=50, unique=True, null=False, blank=False)
     description = models.TextField(max_length=2000, null=False, blank=False)
     slug =models.SlugField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cruise(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=False, blank=False)
+
+    destinations = models.ManyToManyField(Destination, related_name = 'destinations')
+
+    def __str__(self) -> str:
+        return self.name
